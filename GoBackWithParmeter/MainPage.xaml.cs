a4e8c0cf163891a91c00b39b111118855fba8844
Nico Zhu <v-zhumin@microsoft.com>
2018-07-09 16:00:56 +0800

    Add project files.

---
+++ GoBackWithParmeter/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace GoBackWithParmeter
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+         Action<string> CallBack;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.RegisterAction( (s) =>
+            {           
+                Info.Text = s;
+            });
+        }
+
+        public void RegisterAction(Action<string> callback)
+        {
+            CallBack = callback;
+        }
+
+        public void InvokeAction(string data)
+        {
+            if (CallBack == null || data == null)
+            {
+                return;
+            }
+            CallBack.Invoke(data);
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private string test;
+        public string Test
+        {
+            get
+            {
+                return test;
+            }
+            set
+            {
+                test = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+          
+            base.OnNavigatedTo(e);
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(ScanerPage),this);
+        }
+    }
+}
---
+++ GoBackWithParmeter/ScanerPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace GoBackWithParmeter
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ScanerPage : Page
+    {
+    
+        public ScanerPage()
+        {
+            this.InitializeComponent();
+        }
+        private MainPage mainPage;
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            mainPage = e.Parameter as MainPage;
+           
+            base.OnNavigatedTo(e);
+           
+        }
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {   
+            base.OnNavigatingFrom(e);
+            mainPage.InvokeAction("Hello Word");
+        }
+
+        private  void Button_Click(object sender, RoutedEventArgs e)
+        {
+           this.Frame.GoBack();
+        }
+    }
+}
